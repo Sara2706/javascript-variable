1d7f389ff7c5a08b781b8a50e962995a827ae923
Sara2706 <101634428+Sara2706@users.noreply.github.com>
2022-05-13 07:18:10 -0700

    Add files via upload

---
+++ Assignment-01/script.js
@@ -0,0 +1,8 @@
+// content
+let content = "Hello";
+
+{
+    let content = "Hello javaScript";
+}
+//  output
+document.getElementById("para").innerHTML=content;
---
+++ Assignment-02/script.js
@@ -0,0 +1,21 @@
+// point-1
+
+// mobiles list
+
+const mobiles = ["iphone" , "Redmi" , "oppo"]
+
+// connect in html
+
+ document.getElementById("point-1").innerHTML=mobiles
+
+//  point-2
+
+const mobiles2 = ["oneplus" , "Redmi" , "oppo"]
+
+// push
+
+mobiles2.push("moto");
+
+// connect in html
+
+document.getElementById("point-2").innerHTML=mobiles2
